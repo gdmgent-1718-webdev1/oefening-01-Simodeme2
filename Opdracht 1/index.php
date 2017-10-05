8ab5212d0ef701e33ef8fa5db1084e92bbc847f2
Simodeme2 <Simodeme2@student.arteveldehs.be>
2017-10-05 10:42:59 +0200

    Update Git Reponsitory

---
+++ Opdracht 1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Oefening 1</title>
+</head>
+
+<body>
+<?php
+    $naam = "Simon";
+
+    echo "<h1>Welkom $naam</h1>";
+?>
+</body>
+</html>
